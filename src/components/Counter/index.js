54a113dcdf3ea0088153894858174bc593b6ec60
Ftnt <63697694+Ftnt@users.noreply.github.com>
2021-12-08 22:31:21 -0500

    feat: conditional rendering IF case

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -5,11 +5,16 @@ import "./Counter.css";
 
 export const Counter = () => {
 
-  const [counter, setcounter] = useState(0)
+  const [counter, setcounter] = useState(0);
+
+  let colorCount = 'text-odd';
+  if (counter % 2 === 0) {
+    colorCount = 'text-even';
+  }
 
   return (
     <div>
-      <p className={counter % 2 === 0 ? 'text-even' : 'text-odd'}> Counter: {counter} </p>
+      <p className={colorCount}> Counter: {counter} </p>
       <button className="btn" onClick={() => { setcounter(counter + 1) }}>+1 </button>
       <button className="btn" onClick={() => { setcounter(counter - 1) }}>-1</button>
     </div>
